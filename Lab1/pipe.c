9bd58f3c69306d41f9f528468bbb963fae8f83c5
Kelvin Jiang <kelvinkxjiang@gmail.com>
2024-05-02 20:08:40 -0700

    implemented pipe.c

--- Lab1/pipe.c
+++ Lab1/pipe.c
@@ -3,9 +3,81 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
-	// TODO: it's all yours
+	if (argc == 1) {
+		perror("must contain at least 1 argument");
+		return EINVAL;
+	}
+
+	errno = 0;
+	int pfd[2];
+	int i;
+
+	for (i = 1; i < argc - 1; i++) {
+		// create pipe
+		if (pipe(pfd) == -1) {
+			perror("error: pipe");
+			return errno;
+		}
+
+		// create fork
+		pid_t pid = fork();
+		if (pid == -1) {
+			perror("error: fork");
+			return errno;
+		}
+
+		if (pid == 0) {
+			// child process
+
+			// close read pipe
+			close(pfd[0]);
+
+			// redirect stdout to write
+			if (dup2(pfd[1], 1) == -1) {
+				perror("error: dup2");
+				return errno;
+			}
+
+			// close write pipe
+			close(pfd[1]);
+
+			// run commands
+			if (execlp(argv[i], argv[i], NULL) == -1) {
+				perror("error: execlp");
+				return errno;
+			}
+		}
+		else {
+			// parent process
+
+			// close write pipe
+			close(pfd[1]);
+
+			// redirect stdin to read
+			if (dup2(pfd[0], 0) == -1) {
+				perror("error: dup2");
+				return errno;
+			}
+
+			// close read pipe
+			close(pfd[0]);
+
+			if (wait(NULL) == -1) {
+				perror("error: wait");
+				return errno;
+			}
+		}
+	}
+
+	// run last command
+	if (execlp(argv[i], argv[i], NULL) == -1) {
+		perror("error: execlp");
+		return errno;
+	}
+
 	return 0;
 }
